a0ce6bc798869f4da74d4eb3d29a2055b119f71d
Michel <michel@MacBook-Pro-de-Michel.local>
2020-07-08 14:05:34 -0400

    Added Error and Status page, imported in stories

--- stories/index.js
+++ stories/index.js
@@ -24,6 +24,12 @@ import Empty from "components/Appointment/Empty";
 
 import Show from "components/Appointment/Show";
 
+import Confirm from "components/Appointment/Confirm"
+
+import Status from "components/Appointment/Status";
+
+import Error from "components/Appointment/Error";
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -156,4 +162,19 @@ storiesOf("Appointment", module)
      onEdit={action("onEdit")} 
      onDelete={action("onDelete")}
      />)
-  .add ("Confirm",() => <Confirm message="Delete the appointment?" onCancel={action("onCancel")} onConfirm={action("onConfirm")}/>)
+  .add ("Confirm",() => 
+  <Confirm 
+    message="Delete the appointment?" 
+    onConfirm={action("onConfirm")}
+    onCancel={action("onCancel")} 
+    />)
+  
+  .add ("Deleting", () =>
+  <Status
+    message="Deleting"
+  />)
+  .add ("Error", () =>
+  <Error
+    message="Could not delete appointment"
+    onClose={action("onClose")}
+    />)
