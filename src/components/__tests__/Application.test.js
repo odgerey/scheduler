db3a95d27d3533e3807da988ec43498b780a28bc
Michel <michel@MacBook-Pro-de-Michel.local>
2020-07-15 16:05:49 -0400

    Updated Application test code

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -2,13 +2,19 @@ import React from "react";
 
 // import "@testing-library/jest-dom"
 
-import {render, cleanup} from '@testing-library/react'
+import {render, fireEvent, cleanup, screen, waitForElement, getByText } from '@testing-library/react'
 
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected.", () => {
-  render(<Application />);
+it("changes the schedule when a new day is selected", async () => {
+  const { getByText } = render(<Application />);
+
+  await waitForElement(() => getByText("Monday"));
+
+  fireEvent.click(getByText("Tuesday"));
+
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
 });
