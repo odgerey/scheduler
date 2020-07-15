b575296a7c871245259a2ce200f3433e39410ed9
Michel <michel@MacBook-Pro-de-Michel.local>
2020-07-15 18:11:42 -0400

    Debugged two dependency version descreptencies: react-testes react-test-renderer && react-jest-dom-fourteen script text

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -86,7 +86,9 @@ export default function Appointment (props) {
   return (
     <> 
       <Header time={props.time}/>
-      <article className="appointment">
+      <article 
+        className="appointment"
+        data-testid="appointment">
         {mode === EMPTY && (
           <Empty 
             onAdd={onAdd} 
--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -2,19 +2,31 @@ import React from "react";
 
 // import "@testing-library/jest-dom"
 
-import {render, fireEvent, cleanup, screen, waitForElement, getByText } from '@testing-library/react'
-
+import {render, fireEvent, screen, cleanup, waitForElement, getByText, getByAltText, getAllByTestId, getByPlaceholderText, prettyDOM } from '@testing-library/react'
 
 import Application from "components/Application";
 
 afterEach(cleanup);
+describe("Application", () => {
+  it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+    await waitForElement(() => getByText("Monday"));
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
 
-it("changes the schedule when a new day is selected", async () => {
-  const { getByText } = render(<Application />);
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+  
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = getAllByTestId(container, "appointment")[0];
+    fireEvent.click(getByAltText(appointment, "Add"));
 
-  await waitForElement(() => getByText("Monday"));
-
-  fireEvent.click(getByText("Tuesday"));
-
-  expect(getByText("Leopold Silvers")).toBeInTheDocument();
-});
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    fireEvent.click(getByText(appointment, "Save"));
+  });
+})
